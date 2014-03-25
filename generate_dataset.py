d5039cd59c91c0a1d553d3c0d8bd8392477eb856
Michael Lindemuth <lindemuth@gmail.com>
2014-03-25 17:00:34 -0400

    Improved dataset generator

--- generate_dataset.py
+++ generate_dataset.py
@@ -11,17 +11,28 @@ def main():
         description="Produce a dataset from targeted glider datatypes"
     )
     parser.add_argument(
-        '--timestamp_file',
+        '--timestamp_field',
         help='Field to use for timestamp',
         default='m_present_time-timestamp'
     )
     parser.add_argument(
-        'glider_data_file',
-        help='Path to glider data file'
+        '--all_rows',
+        help=(
+            'Produce a CSV file with all datapoint rows and  NaNs where data '
+            'is not available.'
+        ),
+        dest="all_rows",
+        action="store_true"
+    )
+    parser.set_defaults(all_rows=False)
+    parser.add_argument(
+        '--output_csv_path',
+        help='Path for output CSV',
+        default='./output.csv'
     )
     parser.add_argument(
-        'output_csv_path',
-        help='Path for output CSV'
+        'glider_data_file',
+        help='Path to glider data file'
     )
     parser.add_argument(
         'glider_parameters',
@@ -40,22 +51,25 @@ def main():
         [filename]
     )
 
-    parameters = [args.timestamp_file]
+    parameters = [args.timestamp_field]
     parameters += args.glider_parameters
 
     with open(args.output_csv_path, 'w') as f:
         writer = csv.DictWriter(
             f,
             parameters,
-            restval='',
+            restval='NaN',
             extrasaction='ignore'
         )
 
         for line in reader:
-            for parameter in args.glider_parameters:
-                if parameter in line:
-                    writer.writerow(line)
-                    break
+            if args.all_rows:
+                writer.writerow(line)
+            else:
+                for parameter in args.glider_parameters:
+                    if parameter in line:
+                        writer.writerow(line)
+                        break
 
 
 if __name__ == '__main__':
